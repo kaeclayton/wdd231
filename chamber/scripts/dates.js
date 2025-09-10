e0e05a9605dc8bf71f859a6168a2d365bb5e3a39
kaeclayton <kclayton@byupathway.edu>
2025-09-10 11:01:42 -0700

    Chamber of Commerce updates 9/10

---
+++ chamber/scripts/dates.js
@@ -0,0 +1,22 @@
+const copyrightYear = document.querySelector('#currentYear');
+const lastModifiedElement = document.querySelector('#lastModified');
+
+if (copyrightYear) {
+    const currentYear = new Date().getFullYear();
+    copyrightYear.textContent = currentYear;
+}
+
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = `Last Modified: ${formatDate(document.lastModified)}`;
+}
+
+function formatDate(dateString) {
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    };
+    return new Date(dateString).toLocaleDateString('en-US', options);
+}
